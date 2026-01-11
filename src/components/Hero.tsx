97caea655adfa401bbc2256971c473bf0246718e
Aaron Leeb <156447042+aaron-leeb@users.noreply.github.com>
2026-01-10 18:25:22 -0600

    Updates to hero and navbar components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Hero = () => {
     return (
-        <div className='bg-black text-white text-center py-16'>
+        <div className='bg-zinc-800 text-white text-center py-16'>
             <img src="" alt="Hero" className='mx-auto mb-8 w-48 rounded-full object-cover
             transform transition-transform duration-300 hover:scale-105'/>
             <h1 className='text-4xl font-bold'>
@@ -12,13 +12,16 @@ const Hero = () => {
                 , a Computer Science student 
                 pursuing a career as a Software Developer.
             </h1>
-            <p className='mt-4 text-lg text-gray-300 px-4 md:px-32'>
+            <p className='mt-4 text-lg text-gray-100 px-4 md:px-32'>
                 I am interested in contributing to a company focused on
                 innovative and impactful software solutions.
             </p>
-            <div>
-                <button>View My Work</button>
-                <button>Download Resume</button>
+            <div className='mt-8 space-x-4'>
+                <button
+                className='bg-gradient-to-r from-blue-400 to-blue-900 text-white hidden md:inline
+                transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>View My Work</button>
+                <button className='bg-gradient-to-r from-blue-400 to-blue-900 text-white hidden md:inline
+                transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Download Resume</button>
             </div>
         </div>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
+        <nav className='bg-zinc-800 text-white px-8 md:px-16 lg:px-24'>
             <div className='container py-2 flex justify-center md:justify-between items-center'>
-                <div className='text-2xl font-bold hidden md:inline'>Aaron Leeb</div>
+                <div className='text-2xl font-bold hidden md:inline'>Aaron</div>
                 <div className='space-x-6'>
-                    <a href="#about" className='hover:text-gray-400'>About</a>
-                    <a href="#skills" className='hover:text-gray-400'>Skills</a>
-                    <a href="#experience" className='hover:text-gray-400'>Experience</a>
-                    <a href="#projects" className='hover:text-gray-400'>Projects</a>
+                    <a href="#about" className='hover:text-gray-100'>About</a>
+                    <a href="#skills" className='hover:text-gray-100'>Skills</a>
+                    <a href="#experience" className='hover:text-gray-100'>Experience</a>
+                    <a href="#projects" className='hover:text-gray-100'>Projects</a>
                 </div>
                 <button className='bg-gradient-to-r from-blue-400 to-blue-900 text-white hidden md:inline
                 transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect</button>
